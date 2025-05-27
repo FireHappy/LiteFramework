9a7905f98a1b612982a3bf9185cf374e28acde36
lin.chen <lin.chen@rokid.com>
2025-05-27 15:46:29 +0800

    Update LoginView.Auto.cs

--- Assets/Sample/Scripts/Features/Login/LoginView.Auto.cs
+++ Assets/Sample/Scripts/Features/Login/LoginView.Auto.cs
@@ -1,19 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using LiteFramework.Utility;
-using LiteFramework.Core.MVP;
 using LiteFramework.Core.Module.UI;
 using TMPro;
 namespace LiteFramework.Sample
 {
-    public partial class LoginView : BaseUIView<LoginPresenter>
-    {
-    	[Autowrited("input_username")] public TMP_InputField inputUserName;
-	[Autowrited("input_password")] public TMP_InputField inputPassword;
-	[Autowrited("btn_login")] public Button btnLogin;
-	[Autowrited("btn_cancel")] public Button btnCancel;
+	public partial class LoginView : BaseUIView<LoginPresenter>
+	{
+		[Autowrited("input_username")] public TMP_InputField inputUserName;
+		[Autowrited("input_password")] public TMP_InputField inputPassword;
+		[Autowrited("btn_login")] public Button btnLogin;
+		[Autowrited("btn_cancel")] public Button btnCancel;
 
-    }
+	}
 }
 
 

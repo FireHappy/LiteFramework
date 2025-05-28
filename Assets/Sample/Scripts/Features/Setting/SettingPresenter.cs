d000a767d0fd9aa055415b5d81518dde2995374c
lin.chen <lin.chen@rokid.com>
2025-05-28 19:13:23 +0800

    [feat] 修改了挺多的

--- Assets/Sample/Scripts/Features/Setting/SettingPresenter.cs
+++ Assets/Sample/Scripts/Features/Setting/SettingPresenter.cs
@@ -4,6 +4,7 @@ using LiteFramework.Module.UI;
 using LiteFramework.Core.Module.UI;
 using VContainer;
 using UnityEngine;
+using UniRx;
 
 namespace LiteFramework.Sample
 {
@@ -18,7 +19,17 @@ namespace LiteFramework.Sample
 
         protected override void OnViewReady()
         {
-
+            view.btnExit.OnClickAsObservable().Subscribe(_ =>
+            {
+                router.Close<SettingView>(UIType.Dialog);
+            }).AddTo(view);
+            view.btnChange.OnClickAsObservable().Subscribe(_ =>
+            {
+                UserModel userModel = container.Resolve<UserModel>();
+                userModel.userName.Value = view.inputUserName.text;
+                userModel.password = view.inputPassword.text;
+                router.Close<SettingView>(UIType.Dialog);
+            }).AddTo(view);
         }
 
         protected override void OnViewDispose()
--- Assets/Sample/Scripts/Features/Setting/SettingView.Auto.cs
+++ Assets/Sample/Scripts/Features/Setting/SettingView.Auto.cs
@@ -5,22 +5,21 @@ using LiteFramework.Core.Module.UI;
 using TMPro;
 namespace LiteFramework.Sample
 {
-    public partial class SettingView : BaseUIView<SettingPresenter>
-    {
+	public partial class SettingView : BaseUIView<SettingPresenter>
+	{
 		public TMP_InputField inputUserName;
 		public TMP_InputField inputPassword;
 		public Button btnChange;
 		public Button btnExit;
 
-        public override void InitComponents()
-        {
+		public override void InitComponents()
+		{
 			inputUserName = transform.Find("Bg/Input_UserName").GetComponent<TMP_InputField>();
 			inputPassword = transform.Find("Bg/Input_Password").GetComponent<TMP_InputField>();
 			btnChange = transform.Find("Bg/Btn_Change").GetComponent<Button>();
 			btnExit = transform.Find("Bg/Btn_Exit").GetComponent<Button>();
-
-        }
-    }
+		}
+	}
 }
 
 

75a827fb090a49278a2b07bfa971ddb9e018a86d
lin.chen <lin.chen@rokid.com>
2025-06-02 16:32:26 +0800

    [feat] 完成项目整理

--- Assets/LiteFramework/Editor/UIModule/UICodeGenerator.cs
+++ Assets/LiteFramework/Editor/UIModule/UICodeGenerator.cs
@@ -2,137 +2,141 @@ using UnityEngine;
 using UnityEditor;
 using System.IO;
 using System.Text;
-using LiteFramework.EditorTools;
 using System;
 using System.Collections.Generic;
 using LiteFramework.Configs;
 
-public static class UIPrefabCodeGenerator
+namespace LiteFramework.EditorTools
 {
-    [MenuItem("Assets/Generate UI MVP Template", true)]
-    private static bool ValidateGenerate()
+    public static class UIPrefabCodeGenerator
     {
-        var obj = Selection.activeObject;
-        return obj is GameObject && AssetDatabase.GetAssetPath(obj).EndsWith(".prefab");
-    }
-
-    [MenuItem("Assets/Generate UI MVP Template")]
-    private static void GenerateUIMVP()
-    {
-        var go = Selection.activeObject as GameObject;
-        string uiName = go.name.Replace("View", "");
-
-        var config = LoadUIGeneratorConfig();
-        if (config == null)
+        [MenuItem("Assets/Generate UI MVP Template", true)]
+        private static bool ValidateGenerate()
         {
-            Debug.LogError("Can't Find UIGeneratorConfig");
-            return;
+            var obj = Selection.activeObject;
+            return obj is GameObject && AssetDatabase.GetAssetPath(obj).EndsWith(".prefab");
         }
 
-        // 路径
-        string viewTemplatePath = Path.Combine(config.templateRootPath, "UIViewTemplate.txt");
-        string viewAutoTemplatePath = Path.Combine(config.templateRootPath, "UIViewAutoTemplate.txt");
-        string presenterTemplatePath = Path.Combine(config.templateRootPath, "UIPresenterTemplate.txt");
-        string viewTemplate = File.ReadAllText(viewTemplatePath);
-        string viewAutoTemplate = File.ReadAllText(viewAutoTemplatePath);
-        string presenterTemplate = File.ReadAllText(presenterTemplatePath);
-
-        // 自动字段生成
-        var (fields, fieldsFind) = GenerateComponentFields(go.transform, config);
-
-        var nameSpace = config.nameSpace;
-
-        // 内容替换
-        string viewCode = viewTemplate
-            .Replace("{UI_NAME}", uiName)
-            .Replace("{NAMESPACE}", nameSpace);
-
-        string viewAutoCode = viewAutoTemplate
-            .Replace("{UI_NAME}", uiName)
-            .Replace("{Fields}", fields)
-            .Replace("{FieldsFind}", fieldsFind)
-            .Replace("{NAMESPACE}", nameSpace);
-
-        string presenterCode = presenterTemplate
-            .Replace("{UI_NAME}", uiName)
-            .Replace("{NAMESPACE}", nameSpace);
-
-        // 写入文件
-        string outputDir = Path.Combine(config.outputRootPath, uiName);
-        Directory.CreateDirectory(outputDir);
-
-        //autoView ui 每次改动都会重新覆盖生成
-        File.WriteAllText(Path.Combine(outputDir, $"{uiName}View.Auto.cs"), viewAutoCode);
-
-        var viewFile = Path.Combine(outputDir, $"{uiName}View.cs");
-        if (!File.Exists(viewFile))
+        [MenuItem("Assets/Generate UI MVP Template")]
+        private static void GenerateUIMVP()
         {
-            File.WriteAllText(Path.Combine(outputDir, $"{uiName}View.cs"), viewCode);
+            var go = Selection.activeObject as GameObject;
+            string uiName = go.name.Replace("View", "");
+
+            var config = LoadUIGeneratorConfig();
+            if (config == null)
+            {
+                Debug.LogError("Can't Find UIGeneratorConfig");
+                return;
+            }
+
+            // 路径
+            string viewTemplatePath = Path.Combine(config.templateRootPath, "UIViewTemplate.txt");
+            string viewAutoTemplatePath = Path.Combine(config.templateRootPath, "UIViewAutoTemplate.txt");
+            string presenterTemplatePath = Path.Combine(config.templateRootPath, "UIPresenterTemplate.txt");
+            string viewTemplate = File.ReadAllText(viewTemplatePath);
+            string viewAutoTemplate = File.ReadAllText(viewAutoTemplatePath);
+            string presenterTemplate = File.ReadAllText(presenterTemplatePath);
+
+            // 自动字段生成
+            var (fields, fieldsFind) = GenerateComponentFields(go.transform, config);
+
+            var nameSpace = config.nameSpace;
+
+            // 内容替换
+            string viewCode = viewTemplate
+                .Replace("{UI_NAME}", uiName)
+                .Replace("{NAMESPACE}", nameSpace);
+
+            string viewAutoCode = viewAutoTemplate
+                .Replace("{UI_NAME}", uiName)
+                .Replace("{Fields}", fields)
+                .Replace("{FieldsFind}", fieldsFind)
+                .Replace("{NAMESPACE}", nameSpace);
+
+            string presenterCode = presenterTemplate
+                .Replace("{UI_NAME}", uiName)
+                .Replace("{NAMESPACE}", nameSpace);
+
+            // 写入文件
+            string outputDir = Path.Combine(config.outputRootPath, uiName);
+            Directory.CreateDirectory(outputDir);
+
+            //autoView ui 每次改动都会重新覆盖生成
+            File.WriteAllText(Path.Combine(outputDir, $"{uiName}View.Auto.cs"), viewAutoCode);
+
+            var viewFile = Path.Combine(outputDir, $"{uiName}View.cs");
+            if (!File.Exists(viewFile))
+            {
+                File.WriteAllText(Path.Combine(outputDir, $"{uiName}View.cs"), viewCode);
+            }
+
+            var presenterFile = Path.Combine(outputDir, $"{uiName}Presenter.cs");
+            if (!File.Exists(presenterFile))
+            {
+                File.WriteAllText(Path.Combine(outputDir, $"{uiName}Presenter.cs"), presenterCode);
+            }
+
+            AssetDatabase.Refresh();
+            UIRouterGeneratorEditor.GenerateRouterFiles(config.outputRootPath);
+            AssetDatabase.Refresh();
+            EditorUtility.DisplayDialog("Generate success!!!", $"Generate {uiName}  MVP code success.", "Sure");
         }
 
-        var presenterFile = Path.Combine(outputDir, $"{uiName}Presenter.cs");
-        if (!File.Exists(presenterFile))
+        private static (string, string) GenerateComponentFields(Transform root, UIGeneratorConfig config)
         {
-            File.WriteAllText(Path.Combine(outputDir, $"{uiName}Presenter.cs"), presenterCode);
+            StringBuilder fields = new StringBuilder();
+            StringBuilder fieldsFind = new StringBuilder();
+            List<string> usedNames = new List<string>();
+
+            foreach (Transform child in root.GetComponentsInChildren<Transform>(true))
+            {
+                string name = child.name;
+                string[] parts = name.Split('_');
+                if (parts.Length < 2) continue;
+
+                string prefix = parts[0];
+                string filedName = parts[1];
+
+                Type type = config.GetComponentTypeFromPrefix(prefix);
+                if (type == null) continue;
+
+                var component = child.GetComponent(type);
+                if (component == null) continue;
+
+                string varName = prefix + filedName;
+                filedName = prefix.ToLower() + filedName;
+                if (usedNames.Contains(varName)) continue;
+                usedNames.Add(varName);
+
+                fields.AppendLine($"\t\tpublic {type.Name} {filedName};");
+                Debug.Log("fieldName:" + filedName);
+                fieldsFind.AppendLine($"\t\t\t{filedName} = transform.Find(\"{GetFindPath(root, child)}\").GetComponent<{type.Name}>();");
+            }
+
+            return (fields.ToString(), fieldsFind.ToString());
         }
 
-        AssetDatabase.Refresh();
-        UIRouterGeneratorEditor.GenerateRouterFiles(config.outputRootPath);
-        AssetDatabase.Refresh();
-        EditorUtility.DisplayDialog("Generate success!!!", $"Generate {uiName}  MVP code success.", "Sure");
-    }
-
-    private static (string, string) GenerateComponentFields(Transform root, UIGeneratorConfig config)
-    {
-        StringBuilder fields = new StringBuilder();
-        StringBuilder fieldsFind = new StringBuilder();
-        List<string> usedNames = new List<string>();
-
-        foreach (Transform child in root.GetComponentsInChildren<Transform>(true))
+        private static string GetFindPath(Transform root, Transform child)
         {
-            string name = child.name;
-            string[] parts = name.Split('_');
-            if (parts.Length < 2) continue;
-
-            string prefix = parts[0];
-            string filedName = parts[1];
-
-            Type type = config.GetComponentTypeFromPrefix(prefix);
-            if (type == null) continue;
-
-            var component = child.GetComponent(type);
-            if (component == null) continue;
-
-            string varName = prefix + filedName;
-            filedName = prefix.ToLower() + filedName;
-            if (usedNames.Contains(varName)) continue;
-            usedNames.Add(varName);
-
-            fields.AppendLine($"\t\tpublic {type.Name} {filedName};");
-            Debug.Log("fieldName:" + filedName);
-            fieldsFind.AppendLine($"\t\t\t{filedName} = transform.Find(\"{GetFindPath(root, child)}\").GetComponent<{type.Name}>();");
+            string path = $"{child.name}";
+            var parent = child.parent;
+            while (parent != root)
+            {
+                parent = parent.parent;
+                path = child.parent.name + '/' + path;
+            }
+            return path;
         }
 
-        return (fields.ToString(), fieldsFind.ToString());
-    }
-
-    private static string GetFindPath(Transform root, Transform child)
-    {
-        string path = $"{child.name}";
-        var parent = child.parent;
-        while (parent != root)
+        private static UIGeneratorConfig LoadUIGeneratorConfig()
         {
-            parent = parent.parent;
-            path = child.parent.name + '/' + path;
+            string[] guids = AssetDatabase.FindAssets("t:UIGeneratorConfig");
+            if (guids.Length == 0) return null;
+            string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+            return AssetDatabase.LoadAssetAtPath<UIGeneratorConfig>(path);
         }
-        return path;
     }
 
-    private static UIGeneratorConfig LoadUIGeneratorConfig()
-    {
-        string[] guids = AssetDatabase.FindAssets("t:UIGeneratorConfig");
-        if (guids.Length == 0) return null;
-        string path = AssetDatabase.GUIDToAssetPath(guids[0]);
-        return AssetDatabase.LoadAssetAtPath<UIGeneratorConfig>(path);
-    }
 }
+
